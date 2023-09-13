279eadd52ead5e4472201836ab31d404f41ac1cc
natadekoko <60341975+natadekoko-rin@users.noreply.github.com>
2023-09-13 07:30:57 +0700

    feat : add Stream Builder

--- lib/bloc/counter.dart
+++ lib/bloc/counter.dart
@@ -2,5 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Counter extends Cubit<int> {
-  Counter() : super(0);
+  Counter({this.init = 0}) : super(init); //konstraktor
+
+  int init;
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    // buat pantau error
+
+    super.onError(error, stackTrace);
+    print(error);
+  }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,27 +1,41 @@
+import 'package:belajar_bloc/bloc/counter.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  Counter mycounter = Counter(init: 99);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Block Builder'),
+          title: const Text('Block Builder'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '0',
-              style: const TextStyle(fontSize: 50),
-            ),
+            StreamBuilder(
+                initialData: mycounter.init,
+                stream: mycounter.stream,
+                builder: (context, snapshot) {
+                  return Text(
+                    "${snapshot.data}",
+                    style: const TextStyle(fontSize: 50),
+                  );
+                }),
             SizedBox(height: 50),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                IconButton(onPressed: () {}, icon: Icon(Icons.remove)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.add))
+                IconButton(
+                    onPressed: () {
+                      mycounter.decrement();
+                    },
+                    icon: const Icon(Icons.remove)),
+                IconButton(
+                    onPressed: () {
+                      mycounter.increment();
+                    },
+                    icon: const Icon(Icons.add))
               ],
             )
           ],
