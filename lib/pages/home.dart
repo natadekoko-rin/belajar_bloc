10544f99d0d86538e4b5df4ceeae34c83cc10f05
natadekoko <60341975+natadekoko-rin@users.noreply.github.com>
2023-09-13 09:00:07 +0700

    feat : Bloc Builder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,9 @@
 import 'package:belajar_bloc/bloc/counter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
-  Counter mycounter = Counter(init: 99);
+  Counter mycounter = Counter();
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +14,37 @@ class HomePage extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StreamBuilder(
-                initialData: mycounter.init,
-                stream: mycounter.stream,
-                builder: (context, snapshot) {
-                  return Text(
-                    "${snapshot.data}",
-                    style: const TextStyle(fontSize: 50),
-                  );
-                }),
+            // StreamBuilder(
+            //     initialData: mycounter.init,
+            //     stream: mycounter.stream,
+            //     builder: (context, snapshot) {
+            //       return Text(
+            //         "${snapshot.data}",
+            //         style: const TextStyle(fontSize: 50),
+            //       );
+            //     }),
+
+            // tipe counter dan tipe statenya
+            BlocBuilder<Counter, int>(
+              bloc: mycounter,
+              // akan build widget baru jika trus
+              buildWhen: (prev, curent) {
+                // print(prev);
+                // print(curent);
+                // return false;
+                if (curent % 2 == 0) {
+                  return true;
+                } else {
+                  return false;
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  "$state",
+                  style: const TextStyle(fontSize: 50),
+                );
+              },
+            ),
             SizedBox(height: 50),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
